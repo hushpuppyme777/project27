aacbb1dbaa3631956a1d02cee014e7adccf892cc
hushpuppyme777 <70584863+hushpuppyme777@users.noreply.github.com>
2021-01-06 08:28:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# project27
-project27
+# NC27
---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var rope1,rope2,rope3, rope4,rope5;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	
+  
+}
+
+
+function draw() {
+  
+  
+  
+	
+  
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+
+  	}
+}
+
+
+function drawLine(constraint)
+{
+	bobBodyPosition=constraint.bodyA.position
+	roofBodyPosition=constraint.bodyB.position
+
+	roofBodyOffset=constraint.pointB;
+	
+	roofBodyX=roofBodyPosition.x+roofBodyOffset.x
+	roofBodyY=roofBodyPosition.y+roofBodyOffset.y
+	line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX,roofBodyY);
+}
+
+
+
+
+
+
